8a8a87e202bf12cbbe312cbd9b26935768864b16
fisk810 <marc571m@edu.easj.dk>
2022-05-10 14:09:23 +0200

    First Commit.
    - Created Entities Case, CaseConatiner, Customer, Employee and User
    
    - Created UseCase EditCase
    
    - EditCase can now check if two strings have matching regex' (checkIfStringMatchesInput)
    
    - EditCase can now find the indexes for Cases matching an arbitrary String input in a CaseContainer. (findCaseMatchingInput)
    
    - EditCase can now find specific caseindex' in a list of cases using a list of index' (sortCasesByIndex)
    
    - EditCase now has a get method, that gets all cases matching an arbitrary string input. (getSearchedCases)

--- src/com/company/UseCase/EditCase.java
+++ src/com/company/UseCase/EditCase.java
@@ -7,49 +7,12 @@ import java.util.List;
 
 public class EditCase {
 
-
-    //denne metode skal kører i et while(rs.next) i databasen. metoden fungerer som en slags
-    //pseudo constructor, i og med at den opretter arrayet i EditCase klassen.
-
-
-/*
-    public List<String> createAddressArray(){
-        ArrayList<String> customerAddress = new ArrayList<>();
-        for (int i = 0; i < caseList.size(); i++) {
-            customerAddress.add(caseList.get(i).getAddress());
-            customerAddress.add(caseList.get(i).getZipCode());
-            customerAddress.add(caseList.get(i).getCity());
-        }
-        return customerAddress;
-    }
-
- */
-
     public List<Case> sortCasesByindex(List<Case> cases, List<Integer> indexList){
         ArrayList<Case> sortedCases = new ArrayList<>();
         for (int i = 0; i < indexList.size(); i++) {
             sortedCases.add(cases.get(indexList.get(i)));
         }
         return sortedCases;
-    }
-
-    public void sortStringsAlphabetically(List<String> c)
-    {
-        String temp;
-        int n = c.size();
-        // Sorting strings using bubble sort
-        for (int j = 0; j < n - 1; j++)
-        {
-            for (int i = j + 1; i < n; i++)
-            {
-                if (c.get(j).compareTo(c.get(i)) > 0)
-                {
-                    temp = c.get(j);
-                    c.set(j, c.get(i));
-                    c.set(i, temp);
-                }
-            }
-        }
     }
 
     public List<Case> getSearchedCases(List<Case> cases, String input){
@@ -65,7 +28,6 @@ public class EditCase {
         }
         return matchingCasesIndex;
     }
-
     //skal lægge i general use-case
     public boolean checkIfStringMatchesInput(String s, String input){
             return s.matches("(.*)" + input + "(.*)");
